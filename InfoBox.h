ce8db3fcd6fb3162144c820dbcbfc1bec326c2f9
mrkoldbrandt <mrkoldbrandt@gmail.com>
2022-03-08 11:18:54 +0100

    bah

---
+++ InfoBox.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include <stdlib.h>
+#include <windows.h>
+
+class InfoBox
+{
+
+    // Displays player's score
+    cout << endl;
+    cout << "Score: " << score << endl;
+};
+
